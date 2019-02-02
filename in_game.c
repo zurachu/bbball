d10616b67c66a39c79b522d3f504183887e27b93
zurachu <zurachu@gmail.com>
2019-02-02 12:04:21 +0900

    リプレイモードでゴール後に空のリプレイデータで上書きしてしまっていたので、修正

--- in_game.c
+++ in_game.c
@@ -146,7 +146,7 @@ void InGame_Update(void)
 	
 	if(g_player.state == PlayerState_Goal)
 	{
-		if(pcePadGet() & TRG_A)
+		if(g_play_mode != InGamePlayMode_Replay && (pcePadGet() & TRG_A))
 		{
 			if(!PadLog_IsCompatible(&g_replay_pad_log, g_stage, g_stage_number) || g_game_frame_count <= g_replay_pad_log.total_frame)
 			{
